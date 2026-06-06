02f8db04ab42019e2c506ca1868070d42db254d9
Juanes <juanestebanortizbejarano@gmail.com>
2026-06-06 14:38:17 -0500

    Cambios finales y merge de hu 1,2,3 y 4

--- app/src/main/java/com/example/picobotella/ui/screens/Fragment.kt
+++ app/src/main/java/com/example/picobotella/ui/screens/Fragment.kt
@@ -1,2 +1,9 @@
 package com.example.picobotella.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Fragment(navController: NavHostController) {
+
+}
